37c181a83e1221bbcf7c1d983438f876fc57d2f9
QuisWill <williamsmarquisa@gmail.com>
2025-09-25 09:51:52 -0400

    Another School project

---
+++ Lab5C.py
@@ -0,0 +1,9 @@
+plz=(input('If you would like to stop this program, say "please":'))
+
+while True:
+    if plz=='please':
+        print("\nProgram complete")
+        break
+    else:
+        plz=(input('\nIf you would like to stop this program, say "please":'))
+
